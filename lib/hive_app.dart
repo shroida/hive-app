5a2cb3856116f859fd74a0e494154fa2936c5898
shroida <shroida012@gmail.com>
2025-02-24 11:01:29 +0200

    convert hive app to stateless widget

--- lib/hive_app.dart
+++ lib/hive_app.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animation/screens/home_screen.dart';
 
-class HiveApp extends StatefulWidget {
+class HiveApp extends StatelessWidget {
   const HiveApp({super.key});
 
-  @override
-  HiveAppState createState() => HiveAppState();
-}
-
-class HiveAppState extends State<HiveApp> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:HomeScreen(),
+      home: HomeScreen(),
     );
   }
 }
